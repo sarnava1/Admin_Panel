a55265a297fe9462656d6649ab16c83567dcb084
sarnava1 <konarsarnava@gmail.com>
2019-03-19 03:38:06 +0530

    added comments for better understanding

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,1 +1,2 @@
+//the various action types are listed here
 export const TEST_DISPATCH = 'TEST_DISPATCH';
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,11 +1,14 @@
+//importing the dependancies
 import { TEST_DISPATCH } from '../actions/types'; 
 
+//the initial state for the auth reducer
 const initialState = {
     isAuthenticated: false,
     user: {}
 }
 
 export default function(state = initialState, action) {
+    //based on the action type our action will selected
     switch (action.type) {
         case TEST_DISPATCH:
           return {
