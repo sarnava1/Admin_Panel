311cd2bf5115e3d5c2ce618e6d99f83de69f82e6
sarnava1 <konarsarnava@gmail.com>
2019-03-18 16:49:34 +0530

    included the basic layout of the components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+//importing the components
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 class App extends Component {
   render() {
