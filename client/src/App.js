3559a3d9e2c7055b30f8d36c78dda39e8178376e
sarnava1 <konarsarnava@gmail.com>
2019-03-18 20:15:27 +0530

    added the auth components basic structure and included react-router-dom as a dependancy for using the BrowserRouter and Router features

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 //importing the components
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-         <h1>My React App</h1>
-       </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component= { Landing } />
+          <div className="container">
+             <Route exact path="/Register" component= {Register} />
+             <Route exact path="/Login" component= {Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
