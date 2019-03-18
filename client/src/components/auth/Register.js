f1439ad06fbcbc20f61d52684ddbaeeda04d1c71
sarnava1 <konarsarnava@gmail.com>
2019-03-18 23:54:26 +0530

    added the error handling for the input fields of the Register Component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,6 +2,7 @@
 
 //importing the dependancies
 import React, { Component } from 'react';
+import classnames from 'classnames';
 
 class Register extends Component {
   constructor() {
@@ -38,6 +39,9 @@ class Register extends Component {
   }
 
   render() {
+
+    const { errors } = this.state;
+
     return (
       <div className="register">
         <div className="container">
@@ -47,47 +51,70 @@ class Register extends Component {
               <p className="lead text-center">
                 Create your Admin Panel account
               </p>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input
                     type="text"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
                     className="form-control form-control-lg"
                     placeholder="Name"
                     name="name"
                     value={this.state.name}
                     onChange={this.onChange}
                   />
+                  {errors.name && (
+                    <div className="invalid-feedback">{errors.name}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="email"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     className="form-control form-control-lg"
                     placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
-                  
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     className="form-control form-control-lg"
                     placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="password"
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
                     className="form-control form-control-lg"
                     placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onChange}
                   />
+                  {errors.password2 && (
+                    <div className="invalid-feedback">{errors.password2}</div>
+                  )}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
