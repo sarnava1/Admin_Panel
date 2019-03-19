da884b07d90582aefeb8c5c641254b11006d3a2b
sarnava1 <konarsarnava@gmail.com>
2019-03-19 21:06:41 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const app=express();
 
@@ -31,6 +32,16 @@ require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users', users);
+
+//serve static assets if we are in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //Making our port equal to 5000 or assigning it to the dynamic port which will be provided by heroku
 const port = process.env.PORT || 5000;
