6019c52ee8b3dd90e7849d2cf2d352d46896b544
sarnava1 <konarsarnava@gmail.com>
2019-03-20 00:01:13 +0530

    added redux-devtools-extension as a dependancy so that our deployed app runs on all browsers

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,8 @@
 //this is the redux store
 
 //importing the dependancies
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnly';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -15,10 +16,7 @@ const initialState = {};
 const store = createStore(
      rootReducer,
      initialState,
-     compose(
-        applyMiddleware(...middleware),      
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-     )
+     composeWithDevTools(applyMiddleware(...middleware))
 );
      
 
